1d512403fd2014ebdbbae0d71db4b49c1071ed47
Mohammed Sufiyaan Shiakh <119026012+sufiyaanshkh@users.noreply.github.com>
2025-09-07 21:58:07 +0530

    Update app.py

--- agrosense_model/app.py
+++ agrosense_model/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
 from sklearn.ensemble import RandomForestClassifier
 
 st.set_page_config(page_title="🌱 Smart Crop Recommendation", layout="centered")
